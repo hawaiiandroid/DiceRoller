16df2f238f9b66e64347039e4ab3c7afff36aca7
Tim Wagner <tim.wagner@ibykus.de>
2020-06-27 21:30:01 +0200

    - es wurde eine Würfel-Klasse geschrieben, über die eine zufällige Würfelzahl generiert werden kann

--- app/src/main/java/io/github/hawaiiandroid/MainActivity.kt
+++ app/src/main/java/io/github/hawaiiandroid/MainActivity.kt
@@ -3,6 +3,7 @@ package io.github.hawaiiandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -12,8 +13,17 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.rollButton)
 
-        rollButton.setOnClickListener {
-            Toast.makeText(this, "Hallo Welt", Toast.LENGTH_SHORT).show()
-        }
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice() {
+        val diceNumber: TextView = findViewById(R.id.diceNumber)
+        diceNumber.text = Dice(6).roll().toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
