e73e355115e9987af48106606e14e79751b53514
Tim Wagner <tim.wagner@ibykus.de>
2020-06-27 21:18:30 +0200

    - die IDs der Layout-Elemente wurden unbenannt - der Würfel-Knopf wurde mit einem Click Listener versehen

--- app/src/main/java/io/github/hawaiiandroid/MainActivity.kt
+++ app/src/main/java/io/github/hawaiiandroid/MainActivity.kt
@@ -2,10 +2,18 @@ package io.github.hawaiiandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.rollButton)
+
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "Hallo Welt", Toast.LENGTH_SHORT).show()
+        }
     }
 }
